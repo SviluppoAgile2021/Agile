5ef9577765e31caee9e7211376aea3ad90bfbd6e
samfeidon <samuelesturbaa@gmail.com>
2021-12-17 16:41:58 +0100

    update login.php

---
+++ Php/DebugConnectionDB.php
@@ -0,0 +1,19 @@
+<?php
+
+//Negli script in cui si vuole utilizzare la connessione al db bisogna fare include("connectionDB.php");
+//all'inizio dello script. Terminate le operazioni la connessione va chiusa  con mysqli_close($conn); ed
+//eventualmente per terminare lo script in sicurezza con die("Ciao mondo");
+
+$hostname="127.0.0.1";
+$username="root";
+$password="root";
+$dbname="sporteal_agile2021";
+$usertable="Utenti";
+
+$conn = new mysqli($hostname, $username, $password); //oggetto connessione
+
+// Controllo connessione
+if ($conn->connect_error) {
+    die("Connessione fallita: " . $conn->connect_error);
+}
+mysqli_select_db($conn, $dbname); //Selezione del database
--- Php/login.php
+++ Php/login.php
@@ -1,17 +1,18 @@
 <?php
 session_start();
-include("ConnectionDB.php");
 
 class Login
 {
     public function loginUtente($usernameInp, $passwordInp, $debug) : bool
     {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
+        if($debug === TRUE){
+            include("DebugConnectionDB.php");
+        } elseif ($debug === FALSE){
+            include("ConnectionDB.php");
+        }
 
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            $query = "SELECT * FROM Utenti WHERE username = '$username' AND pwd = '$password'";
+            $query = "SELECT * FROM Utenti WHERE username = '$usernameInp' AND pwd = '$passwordInp'";
 
             if (isset($conn)) {
                 $result = mysqli_query($conn, $query);
@@ -21,8 +22,8 @@ class Login
 
                 session_start();
 
-                $_SESSION['username'] = $username;
-                $_SESSION['password'] = $password;
+                $_SESSION['username'] = $usernameInp;
+                $_SESSION['password'] = $passwordInp;
                 $conn->close();
                 header('location: ../pages/welcome.html');  //qui bisogna mettere il path della home
                 return true;
