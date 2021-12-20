65aa6e23872b90183be0e9d3f404a512a176d715
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2021-12-20 20:32:35 +0100

    Modifiche

--- Php/test/CreaEventoTest.php
+++ Php/test/CreaEventoTest.php
@@ -10,10 +10,8 @@ class CreaEventoTest extends TestCase
     {
         $OggettoEvento = new CreaEvento();
         //verifica se l'evento viene creato correttamente
-        $result = $OggettoEvento->creazioneEvento("asdrubale", "1", "1", "0", "1", "1", "250.00", "0", "2021-08-08 20:30");
+        $result = $OggettoEvento->creazioneEvento("asdrubale", "1", "1", "0", "1", "1", "250.00", "0", "2021-08-08 20:30",true);
         $this->assertTrue($result);
-
-
     }
 
 }
