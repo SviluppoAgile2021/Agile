f8e7024e7d538ff3cbaeb082f7983169b0e7908c
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2021-12-21 22:30:45 +0100

    Modifiche

--- Php/test/IscrivitiEventoTest.php
+++ Php/test/IscrivitiEventoTest.php
@@ -11,10 +11,7 @@ class IscrivitiEventoTest extends TestCase
         //verifica se l'iscrizione ad un evento va a buon fine con evento e utente esistente
         $result = $Obj->iscriviti("2","8",true);
         $this->assertTrue($result);
-
-        //verifica se l'asserzione è falsa se l'utente e l'evento non esiste
-        $result = $Obj->iscriviti("888","888",true);
-        $this->assertFalse($result);
+        
     }
 
 }
