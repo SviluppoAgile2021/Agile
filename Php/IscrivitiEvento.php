0abb5a097d2a7c4f0d03b3bcf79602b5d4522782
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2021-12-21 22:28:59 +0100

    Modifiche

--- Php/IscrivitiEvento.php
+++ Php/IscrivitiEvento.php
@@ -15,7 +15,7 @@ class IscrivitiEvento
         $query = " INSERT INTO partecipano (`id_utente`, `id_evento`) VALUES ('$idUtente', '$idEvento');";
         $risultato = mysqli_query($conn, $query);
         if ($risultato === FALSE) {
-            echo(mysqli_error($conn));
+            //echo(mysqli_error($conn));
             mysqli_close($conn);
             return false;
         } else {
