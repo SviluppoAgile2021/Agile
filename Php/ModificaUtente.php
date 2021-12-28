fa6c6d53b6c3159b8655fddbab6ea974ae2f04aa
Edoardo Giammarino <edoardo.giammarino@student.univaq.it>
2021-12-28 12:04:08 +0100

    Remove Old

--- Php/ModificaUtente.php
+++ Php/ModificaUtente.php
@@ -17,9 +17,7 @@ class ModificaUtente
         }
 
         //creazione query dati escape
-        $query = "UPDATE Utenti(nome, cognome, data_nascita, citta, username, pwd) SET ('$nome','$cognome','$data_nascita', '$citta', '$username', '$pwd') WHERE id='$id'";
-        $debugQuery = $query;
-        echo "\n" . $debugQuery;
+        $query = "UPDATE Utenti (nome, cognome, data_nascita, citta, username, pwd) SET ('$nome','$cognome','$data_nascita', '$citta', '$username', '$pwd') WHERE id='$id'";
 
         $res = mysqli_query($conn, $query);
         if ($res === false) {
--- Php/test/ModificaUtenteTest.php
+++ Php/test/ModificaUtenteTest.php
@@ -12,7 +12,7 @@ class ModificaUtenteTest extends TestCase
     {
         $utente = new ModificaUtente();
         //verifica se l'utente esistente viene modificato correttamente
-        $res = $utente->modificaUtente(12,"Nicolas", "Paruolo", "1996-04-12", "Bologna", "utente12", "utente12New", true);
+        $res = $utente->modificaUtente(12,"Nicolas", "Paruolo", "1996-04-12", "Bologna", "utente12", "utente12Nuova", true);
         $this->assertTrue($res);
 
         //verifica sull'utente NON esistente
