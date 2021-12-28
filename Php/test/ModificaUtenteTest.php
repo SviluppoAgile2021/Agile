5b16495656c396e39e6676fe91e94b0bb1209dc7
Edoardo Giammarino <edoardo.giammarino@student.univaq.it>
2021-12-28 11:24:35 +0100

    Update ModificaUtenteTest.php

--- Php/test/ModificaUtenteTest.php
+++ Php/test/ModificaUtenteTest.php
@@ -12,15 +12,15 @@ class ModificaUtenteTest extends TestCase
     {
         $utente = new ModificaUtente();
         //verifica se l'utente esistente viene modificato correttamente
-        $result = $utente->modificaUtente(14, true);
+        $result = $utente->modificaUtente(12,'Nicolas', 'Paruolo', '1996-04-12', 'Bologna', 'utente12', 'utente12New', true);
         $this->assertTrue($result);
 
         //verifica sull'utente NON esistente
-        $result = $utente->modificaUtente(200, true);
+        $result = $utente->modificaUtente(200,'Nicolas', 'Paruolo', '1996-04-12', 'Bologna', 'utente12', 'utente12', true);
         $this->assertFalse($result);
 
         //verifica sull'id "null"
-        $result = $utente->modificaUtente(null, true);
+        $result = $utente->modificaUtente(null, 'Nicolas', 'Paruolo', '1996-04-12', 'Bologna', 'utente12', 'utente12', true);
         $this->assertFalse($result);
     }
 }
