7cd74a475cca61d9596b91b059253bd917a90abb
Edoardo Giammarino <edoardo.giammarino@student.univaq.it>
2021-12-28 11:46:00 +0100

    Update Controller

--- Php/ModificaUtenteController.php
+++ Php/ModificaUtenteController.php
@@ -5,5 +5,5 @@ include("ModificaUtente.php");
     if (isset($_POST["username"])) {
         modificaUtente($_POST["id"], $_POST["nome"], $_POST["cognome"], $_POST["data_nascita"], $_POST["citta"], $_POST["username"], $_POST["pwd"], false);
     } else {
-    echo "Errore: impossibile modificare l'utente";
+    echo "Errore: impossibile modificare l'Utente";
     }
