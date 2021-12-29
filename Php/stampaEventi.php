b914b258c63086c430003357e0f80e9495d021cb
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2021-12-29 12:33:04 +0100

    Impl. gestione policy accesso eventi formali

--- Php/stampaEventi.php
+++ Php/stampaEventi.php
@@ -1,10 +1,11 @@
 <?php
 include("ConnectionDB.php");
-$ris = mysqli_query($conn, "SELECT id,nome FROM Eventi");
+$ris = mysqli_query($conn, "SELECT id,nome,formale FROM Eventi");
 while ($rigaCorrente = $ris->fetch_array()) {
     $nomeEvento = $rigaCorrente['nome'];
     $id = $rigaCorrente['id'];
-    echo "<option value=\"$id\"> $nomeEvento </option>";
+    $formale = $rigaCorrente['formale'];
+    echo "<option value='$id.$formale'> $nomeEvento </option>";
 }
 
 mysqli_close($conn);
